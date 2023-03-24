7b860f452f41e9d46af1f0ed6161f55e0bfd16fa
Stephanie CP <stephers.cp@gmail.com>
2023-03-24 13:17:38 -0700

    One test working still + added comments before troubleshooting home page issue

--- mainPageObject.ts
+++ mainPageObject.ts
@@ -9,10 +9,10 @@ export class DtMainPageObject extends BasePage {
     byHomeLogo: By =By.xpath("//body/header/section/div/a[@class='logo']/img[1]")
 
     testToBeDeletedHome: By =By.className("rbc-toolbar-label") // Delete
-    testToBeDeletedCompany: By =By.xpath("//h3[normalize-space()='Innovation + Impact = DTNA DNA']") // Delete
+    testToBeDeletedCompany: By =By.xpath("//h3[normalize-space()='Innovation + Impact = DTNA DNA']") // Delete - Alt test (works)
 
     constructor(){
-        super({url:"https://northamerica.daimlertruck.com/company/"}) // Delete
+        super({url:"https://northamerica.daimlertruck.com/company/"}) // Delete - Alt test (works)
         // super({url:"https://northamerica.daimlertruck.com/"})
     }
 
@@ -21,16 +21,16 @@ export class DtMainPageObject extends BasePage {
     // homePageVideo: By = By.xpath("/video[@id='html5_video_hv1vsgn8ti']") // Delete?
     async verifyOnTheHomePage() {
         jest.setTimeout(10000)
-        // await this.verifyElementExists(this.byHomePageVideo) // Delete
-        await this.verifyElementExists(this.testToBeDeletedCompany) 
+        // await this.verifyElementExists(this.byHomePageVideo) // Delete - Alt test (works)
+        await this.verifyElementExists(this.testToBeDeletedCompany) // Temp selector
     }
 
     eventsNavListItem: By = By.xpath("//body//header//section//nav//section//ul//li//a[normalize-space()='Events']")
     homeLogo: By = By.xpath("//body/header/section/div/a[@class='logo']/img[1]")
 
-    async VerifyNavItemAndHomeLogoLink() {
-        await this.click(this.eventsNavListItem) // to take in a variable later
-        await this.click(this.homeLogo)
-    }
+    // async VerifyNavItemAndHomeLogoLink() {
+    //     await this.click(this.eventsNavListItem) // to take in a variable later
+    //     await this.click(this.homeLogo) 
+    // }
 
 }
--- mainTest.test.ts
+++ mainTest.test.ts
@@ -9,21 +9,22 @@ beforeAll(async () => {
     console.log("BEFORE NAV")
     await dtMainPageObject.navigate()
     console.log("AFTER NAV")
-    // await dtMainPageObject.driver.manage().window().maximize()
+    await dtMainPageObject.driver.manage().window().maximize()
 })
 
 describe("1- Basic reality check", () => {
     test("1.0 - Can load the home page", async() => {
         console.log("BEFORE verify")
-
         await dtMainPageObject.verifyOnTheHomePage()
         console.log ("(1.0) Loaded home page")
     }) 
+
+    // //Test below won't work ultil able to confirm user is on the home page
     // test("1.1 - Can navigate to nav page and return to home with top left logo", async() => {
     //     await dtMainPageObject.VerifyNavItemAndHomeLogoLink()
     //     await dtMainPageObject.verifyOnTheHomePage()
-    //     console.log ("(1.1) Clicked nav item + returned home")
-    // }) 
+    //    console.log ("(1.1) Clicked nav item + returned home")
+    // })  
 })
 
 afterAll(async () => {
