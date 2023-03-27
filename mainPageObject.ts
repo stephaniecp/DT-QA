8cf8882658192e0f55806af41502dcf0e5f0af38
Stephanie CP <stephers.cp@gmail.com>
2023-03-26 18:03:44 -0700

    Hello world test working

--- mainPageObject.ts
+++ mainPageObject.ts
@@ -6,23 +6,28 @@ export class DtMainPageObject extends BasePage {
     // Selectors below
     byHomePageVideo: By =By.xpath("//video[@id='html5_video_hv1vsgn8ti']") // Verified > $x("//video[@id='html5_video_hv1vsgn8ti']")
     byEventsNavListItem: By =By.xpath("//body//header//section//nav//section//ul//li//a[normalize-space()='Events']")
+    byCompanyNavListItem: By =By.xpath("//body//header//section//nav//section//ul//li//a[normalize-space()='Company']")
     byHomeLogo: By =By.xpath("//body/header/section/div/a[@class='logo']/img[1]")
 
-    testToBeDeletedHome: By =By.className("rbc-toolbar-label") // Delete
-    testToBeDeletedCompany: By =By.xpath("//h3[normalize-space()='Innovation + Impact = DTNA DNA']") // Delete - Alt test (works)
-
     constructor(){
-        super({url:"https://northamerica.daimlertruck.com/company/"}) // Delete - Alt test (works)
-        // super({url:"https://northamerica.daimlertruck.com/"})
+        // super({url:"https://northamerica.daimlertruck.com/company/"}) // Delete - Alt test (works)
+        super({url:"https://northamerica.daimlertruck.com/"}) // Comment out when trying alt test above
     }
 
     // Custom methods below
 
+    testToBeDeletedHome: By =By.className("rbc-toolbar-label") // Delete
+    testToBeDeletedCompany: By =By.xpath("//h3[normalize-space()='Innovation + Impact = DTNA DNA']") // Delete - Alt test (works)
+    async helloWorldTest() {
+        await this.click(this.byCompanyNavListItem)  // Clicks to access Company page
+        await this.verifyElementExists(this.testToBeDeletedCompany)
+        await this.click(this.byHomeLogo) // Clicks to return to home page
+    }
+
     // homePageVideo: By = By.xpath("/video[@id='html5_video_hv1vsgn8ti']") // Delete?
     async verifyOnTheHomePage() {
-        jest.setTimeout(10000)
         // await this.verifyElementExists(this.byHomePageVideo) // Delete - Alt test (works)
-        await this.verifyElementExists(this.testToBeDeletedCompany) // Temp selector
+        // await this.verifyElementExists(this.testToBeDeletedHome) // Temp selector
     }
 
     eventsNavListItem: By = By.xpath("//body//header//section//nav//section//ul//li//a[normalize-space()='Events']")
--- mainTest.test.ts
+++ mainTest.test.ts
@@ -5,6 +5,8 @@ import { DtMainPageObject } from './mainPageObject'
 const dtMainPageObject = new DtMainPageObject()
 // Test single test by using describe/only / or skip with describe.skip
 
+jest.setTimeout(100000) // Move above P.O. const creation if any methods are timing out
+
 beforeAll(async () => {
     console.log("BEFORE NAV")
     await dtMainPageObject.navigate()
@@ -13,11 +15,17 @@ beforeAll(async () => {
 })
 
 describe("1- Basic reality check", () => {
-    test("1.0 - Can load the home page", async() => {
-        console.log("BEFORE verify")
-        await dtMainPageObject.verifyOnTheHomePage()
-        console.log ("(1.0) Loaded home page")
+    test("1.0 - Can verify an element on the Company page", async() => {
+        console.log("1.0 Start")
+        await dtMainPageObject.helloWorldTest()
+        console.log("1.0 End")
     }) 
+
+    // test("1.1 - Can load the home page", async() => {
+    //     console.log("1.1 Start")
+    //     await dtMainPageObject.verifyOnTheHomePage()
+    //     console.log ("1.1 End")
+    // }) 
 
     // //Test below won't work ultil able to confirm user is on the home page
     // test("1.1 - Can navigate to nav page and return to home with top left logo", async() => {
